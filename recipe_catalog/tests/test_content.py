0140b60ea088b6ec835b9fd8ee06668d64200aef
lutchenko a <artiru378@gmail.com>
2024-11-29 17:23:58 +0300

    Add View Context Tests

--- recipe_catalog/tests/test_content.py
+++ recipe_catalog/tests/test_content.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 from decimal import Decimal
 
 class ContentTestCase(TestCase):
+    RECIPE_URL = reverse('recipe_catalog:recipe_detail', args=[1])
     @classmethod
     def setUpTestData(cls):
         # Create test ingredients
@@ -21,6 +22,8 @@ class ContentTestCase(TestCase):
             weight_ready=1000, 
             price=Decimal('0.10')
         )
+
+        cls.ingredients = [cls.flour, cls.water]
         
         # Create multiple test recipes for sorting
         cls.recipes = [
@@ -64,6 +67,36 @@ class ContentTestCase(TestCase):
         recipe = self.recipes[0]
         recipe_ingredients = recipe.ingredients.order_by('name')
         self.assertEqual(list(recipe_ingredients), [self.flour, self.water])
+    
+    # Test recipe view context -------------------------------------------------------------
+    def test_recipes_list_view_context(self):
+        response = self.client.get(reverse('recipe_catalog:index'))
+        self.assertEqual(response.status_code, 200)
+        context = response.context
+        self.assertEqual(len(context['recipes']), len(self.recipes))
+        self.sorted_recipes = sorted(self.recipes, key=lambda recipe: recipe.title)
+        for i, recipe in enumerate(context['recipes']):
+            self.assertEqual(recipe.title, self.sorted_recipes[i].title)
+            self.assertEqual(recipe.cooking_time, self.sorted_recipes[i].cooking_time)
+            self.assertEqual(recipe.description, self.sorted_recipes[i].description)
+
+    def test_recipe_view_context(self):
+        url = reverse('recipe_catalog:recipe_detail', args=[self.recipes[0].id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        context = response.context
+        self.assertEqual(context['recipe_id'], self.recipes[0].id)
+        self.assertEqual(context['title'], self.recipes[0].title)
+        self.assertEqual(context['cooking_time'], self.recipes[0].cooking_time)
+        self.assertEqual(context['description'], self.recipes[0].description)
+        self.assertEqual(len(context['ingredients']), len(self.ingredients))
+        for i, ingredient in enumerate(context['ingredients']):
+            self.assertEqual(ingredient.name, self.ingredients[i].name)
+    
+    def test_recipe_ingredients_count(self):
+        response = self.client.get(self.RECIPE_URL)
+        context = response.context
+        self.assertEqual(len(context['ingredients']), len(self.ingredients))
 
     def test_recipes_alphabetical_order(self):
         """Test recipes are sorted alphabetically on index page"""
